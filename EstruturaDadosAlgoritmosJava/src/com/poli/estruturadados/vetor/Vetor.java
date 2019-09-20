914d03b04542a0e271427c1e93f7b7b31b95f9cc
Polixenia Correia <polixeniacorreia@gmail.com>
2019-09-19 21:36:37 -0300

    Adiciona Elemento no Final do Vetor

--- EstruturaDadosAlgoritmosJava/src/com/poli/estruturadados/vetor/Vetor.java
+++ EstruturaDadosAlgoritmosJava/src/com/poli/estruturadados/vetor/Vetor.java
@@ -3,9 +3,48 @@ package com.poli.estruturadados.vetor;
 public class Vetor {
 	
 	private String [] elementos;
+	private int tamanho;
 	
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		tamanho = 0;
 	}
 	
+//	public void Adicionar(String elemento) {
+//		
+//		for(int i = 0; i < this.elementos.length; i++) {
+//			
+//			if(this.elementos[i] == null) {
+//				this.elementos[i] = elemento;
+//				
+//				System.out.println("" +elementos[i]);
+//				
+//				break;
+//			}
+//		}
+//	}
+	
+//	public void Adicionar(String elemento) throws Exception {
+//		
+//		if(this.tamanho < this.elementos.length) {
+//			this.elementos[this.tamanho] = elemento;
+//			this.tamanho++;
+//			System.out.println("" +elementos[2]);
+//			
+//		}else {
+//			throw new Exception("Não é possível adcionar mais elementos");
+//		}		
+//	}
+	
+	public boolean Adicionar(String elemento){
+		
+		if(this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			System.out.println(" " +this.elementos[this.tamanho]);
+			this.tamanho++;
+			
+		return true;
+		}
+		return false;
+	}
 }
---
+++ EstruturaDadosAlgoritmosJava/src/com/poli/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,17 @@
+package com.poli.estruturadados.vetor.teste;
+
+import com.poli.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(2);
+
+		vetor.Adicionar("Elemento 1");
+		vetor.Adicionar("Elemento 2");
+		vetor.Adicionar("Elemento 3");
+
+	}
+
+}

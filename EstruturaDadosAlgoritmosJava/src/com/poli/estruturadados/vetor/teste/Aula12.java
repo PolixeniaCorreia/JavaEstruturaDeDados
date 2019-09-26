7b4ad81b64db2741371c9fb1123e2b6c886e3463
Polixenia Correia <polixeniacorreia@gmail.com>
2019-09-26 16:35:25 -0300

    ArrayList Funções

---
+++ EstruturaDadosAlgoritmosJava/src/com/poli/estruturadados/vetor/teste/Aula12.java
@@ -0,0 +1,46 @@
+package com.poli.estruturadados.vetor.teste;
+
+import java.util.ArrayList;
+
+public class Aula12 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> arraylist = new ArrayList<String>();
+
+		arraylist.add("A");
+		arraylist.add("C");
+		
+		System.out.println(arraylist);
+		
+		arraylist.add(1, "B");
+		
+		System.out.println(arraylist);
+		
+		boolean existe = arraylist.contains("A");
+		
+		if(existe) {
+			System.out.println("Elemento existe no array");
+		}else {
+			System.out.println("Elemento não existe no array");
+		}
+		
+		int pos = arraylist.indexOf("D");
+		
+		if(pos > -1) {
+			System.out.println("Elemento existe no array" +pos);
+		}else {
+			System.out.println("Elemento não existe no array" +pos);
+		}
+		
+		System.out.println(arraylist.get(2));
+		
+		arraylist.remove(0);
+		arraylist.remove("B");
+		
+		System.out.println(arraylist);
+		System.out.println(arraylist.size());
+		
+	}
+
+}

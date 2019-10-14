635ca524be4a2f996df2f2fb604b242aa49f5a6c
Polixenia Correia <polixeniacorreia@gmail.com>
2019-10-14 14:38:39 -0300

    Enfileirar (adicionar) elemento (enqueue)

--- EstruturaDadosAlgoritmosJava/src/com/poli/estruturadados/fila/Fila.java
+++ EstruturaDadosAlgoritmosJava/src/com/poli/estruturadados/fila/Fila.java
@@ -11,7 +11,12 @@ public class Fila<T> extends EstruturaEstatica<T> {
 	public Fila(int capacidade) {
 		super(capacidade);
 	}
-
-
+	
+	public void enfileira(T elemento) {
+//		this.elementos[this.tamanho] = elemento;
+//		this.tamanho++;
+		
+		this.Adicionar(elemento);
+	}
 
 }
--- EstruturaDadosAlgoritmosJava/src/com/poli/estruturadados/fila/teste/aula20.java
+++ EstruturaDadosAlgoritmosJava/src/com/poli/estruturadados/fila/teste/aula20.java
@@ -2,15 +2,20 @@ package com.poli.estruturadados.fila.teste;
 
 import com.poli.estruturadados.fila.Fila;
 
-public class aula19 {
+public class aula20 {
 
 	public static void main(String[] args) {
-
+		
 		Fila<Integer> fila = new Fila<>();
-
+		
+		fila.enfileira(1);
+		fila.enfileira(2);
+		fila.enfileira(3);
+		
 		System.out.println(fila.estaVazia());
 		System.out.println(fila.tamanho());
-
-
+		System.out.println(fila.toString());
+	
 	}
+
 }
